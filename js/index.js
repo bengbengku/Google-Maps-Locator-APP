6e014f898c7cd42f0f4b2e198ffa905a75bddc18
bengbengku <anggiatbenget70@gmail.com>
2020-10-09 00:14:33 +0700

    ADD: Marker front end

---
+++ js/index.js
@@ -0,0 +1,19 @@
+let map;
+
+function initMap() {
+  let centerIndonesia = {
+    lat: 3.597031,
+    lng: 98.678513,
+  };
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: centerIndonesia,
+    zoom: 16,
+  });
+  var marker = new google.maps.Marker({
+    position: {
+      lat: 3.597031,
+      lng: 98.678513,
+    },
+    map: map,
+  });
+}
